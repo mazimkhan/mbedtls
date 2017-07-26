c0c2b1c228da9b87bc008e1f41aecba0d9fefd1e
Mohammad Azim Khan <Azim.Khan@ARM.com>
2017-07-26 14:08:04 +0100

    Add SERIALISE module to scripts/generate_errors.pl

--- scripts/generate_errors.pl
+++ scripts/generate_errors.pl
@@ -33,7 +33,7 @@ my @low_level_modules = qw( AES ARC4 ASN1 BASE64 BIGNUM BLOWFISH
                             CAMELLIA CCM CMAC CTR_DRBG DES
                             ENTROPY GCM HMAC_DRBG MD2 MD4 MD5
                             NET OID PADLOCK PBKDF2 RIPEMD160
-                            SHA1 SHA256 SHA512 THREADING XTEA );
+                            SHA1 SHA256 SHA512 THREADING XTEA SERIALIZE );
 my @high_level_modules = qw( CIPHER DHM ECP MD
                              PEM PK PKCS12 PKCS5
                              RSA SSL X509 );
