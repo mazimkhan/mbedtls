b736beba536022b2a044d29561af80e008d0af3d
Mohammad Azim Khan <Azim.Khan@arm.com>
2018-04-03 12:26:07 +0100

    Remove dependency on git

--- tests/scripts/ciscript.sh
+++ tests/scripts/ciscript.sh
@@ -99,21 +99,6 @@ msg()
     current_section=$1
 }
 
-# remove built files as well as the cmake cache/config
-cleanup()
-{
-    command make clean
-
-    find . -name yotta -prune -o -iname '*cmake*' -not -name CMakeLists.txt -exec rm -rf {} \+
-    rm -f include/Makefile include/mbedtls/Makefile programs/*/Makefile
-    git update-index --no-skip-worktree Makefile library/Makefile programs/Makefile tests/Makefile
-    git checkout -- Makefile library/Makefile programs/Makefile tests/Makefile
-
-    if [ -f "$CONFIG_BAK" ]; then
-        mv "$CONFIG_BAK" "$CONFIG_H"
-    fi
-}
-
 
 . ./cienv.sh
 check_env TEST_NAME MBEDTLS_ROOT
@@ -127,47 +112,38 @@ cd ${MBEDTLS_ROOT}
 ################################################################
 if [ "X${CONFIG:-X}" != XX ]; then
     if [ "${CONFIG}" = "sslv3" ]; then
-        cleanup
         cp "$CONFIG_H" "$CONFIG_BAK"
         scripts/config.pl set MBEDTLS_SSL_PROTO_SSL3
     elif [ "${CONFIG}" = "no_ssl_renegotiation" ]; then
-        cleanup
         cp "$CONFIG_H" "$CONFIG_BAK"
         scripts/config.pl unset MBEDTLS_SSL_RENEGOTIATION
     elif [ "${CONFIG}" = "full-config-no-mem-backtrace" ]; then
-        cleanup
         cp "$CONFIG_H" "$CONFIG_BAK"
         scripts/config.pl full
         scripts/config.pl unset MBEDTLS_MEMORY_BACKTRACE
     elif [ "${CONFIG}" = "full-config-no-std-func-nv-seed" ]; then
-        cleanup
         cp "$CONFIG_H" "$CONFIG_BAK"
         scripts/config.pl full
         scripts/config.pl set MBEDTLS_PLATFORM_NO_STD_FUNCTIONS
         scripts/config.pl unset MBEDTLS_ENTROPY_NV_SEED
     elif [ "${CONFIG}" = "full-config-no-srv" ]; then
-        cleanup
         cp "$CONFIG_H" "$CONFIG_BAK"
         scripts/config.pl full
         scripts/config.pl set MBEDTLS_SSL_SRV_C
     elif [ "${CONFIG}" = "full-config-no-cli" ]; then
-        cleanup
         cp "$CONFIG_H" "$CONFIG_BAK"
         scripts/config.pl full
         scripts/config.pl set MBEDTLS_SSL_CLI_C
     elif [ "${CONFIG}" = "full-config-no-net-entropy" ]; then
-        cleanup
         cp "$CONFIG_H" "$CONFIG_BAK"
         scripts/config.pl full
         scripts/config.pl unset MBEDTLS_NET_C # getaddrinfo() undeclared, etc.
         scripts/config.pl set MBEDTLS_NO_PLATFORM_ENTROPY # uses syscall() on GNU/Linux
     elif [ "${CONFIG}" = "no-max-fragment-len" ]; then
-        cleanup
         cp "$CONFIG_H" "$CONFIG_BAK"
         scripts/config.pl full
         scripts/config.pl unset MBEDTLS_SSL_MAX_FRAGMENT_LENGTH
     elif [ "${CONFIG}" = "test-null-entropy" ]; then
-        cleanup
         cp "$CONFIG_H" "$CONFIG_BAK"
         scripts/config.pl set MBEDTLS_TEST_NULL_ENTROPY
         scripts/config.pl set MBEDTLS_NO_DEFAULT_ENTROPY_SOURCES
@@ -177,14 +153,12 @@ if [ "X${CONFIG:-X}" != XX ]; then
         scripts/config.pl unset MBEDTLS_HAVEGE_C
 
     elif [ "${CONFIG}" = "bignum-limbs" ]; then
-        cleanup
         cp "$CONFIG_H" "$CONFIG_BAK"
         scripts/config.pl unset MBEDTLS_HAVE_ASM
         scripts/config.pl unset MBEDTLS_AESNI_C
         scripts/config.pl unset MBEDTLS_PADLOCK_C
 
     elif [ "${CONFIG}" = "baremetal" ]; then
-        cleanup
         cp "$CONFIG_H" "$CONFIG_BAK"
         scripts/config.pl full
         scripts/config.pl unset MBEDTLS_NET_C
@@ -200,7 +174,6 @@ if [ "X${CONFIG:-X}" != XX ]; then
         scripts/config.pl unset MBEDTLS_MEMORY_BUFFER_ALLOC_C # calls exit
 
     elif [ "${CONFIG}" = "baremetal-no-udbl" ]; then
-        cleanup
         cp "$CONFIG_H" "$CONFIG_BAK"
         scripts/config.pl full
         scripts/config.pl unset MBEDTLS_NET_C
@@ -217,7 +190,6 @@ if [ "X${CONFIG:-X}" != XX ]; then
         scripts/config.pl set MBEDTLS_NO_UDBL_DIVISION
 
     elif [ "${CONFIG}" = "baremetal-for-arm" ]; then
-        cleanup
         cp "$CONFIG_H" "$CONFIG_BAK"
         scripts/config.pl full
         scripts/config.pl unset MBEDTLS_NET_C
@@ -237,7 +209,6 @@ if [ "X${CONFIG:-X}" != XX ]; then
         scripts/config.pl unset MBEDTLS_PLATFORM_TIME_ALT # depends on MBEDTLS_HAVE_TIME
 
     elif [ "${CONFIG}" = "full-config-no-platform" ]; then
-        cleanup
         cp "$CONFIG_H" "$CONFIG_BAK"
         scripts/config.pl full
         scripts/config.pl unset MBEDTLS_PLATFORM_C
@@ -252,19 +223,15 @@ if [ "X${CONFIG:-X}" != XX ]; then
         scripts/config.pl unset MBEDTLS_MEMORY_BUFFER_ALLOC_C
         scripts/config.pl unset MBEDTLS_FS_IO
     elif [ "${CONFIG}" = "allow-sha1-in-certs" ]; then
-        cleanup
         cp "$CONFIG_H" "$CONFIG_BAK"
         scripts/config.pl set MBEDTLS_TLS_DEFAULT_ALLOW_SHA1_IN_CERTIFICATES
     elif [ "${CONFIG}" = "rsa-no-cert" ]; then
-        cleanup
         cp "$CONFIG_H" "$CONFIG_BAK"
         scripts/config.pl set MBEDTLS_RSA_NO_CRT
     elif [ "${CONFIG}" = "memsan" ]; then
-        cleanup
         cp "$CONFIG_H" "$CONFIG_BAK"
         scripts/config.pl unset MBEDTLS_AESNI_C
     else
-        cleanup
         scripts/config.pl ${CONFIG}
     fi
 fi
@@ -289,7 +256,6 @@ echo "here"
 
     elif [ "$BUILD" = "cmake-asan" ]; then
         check_env CC MAKE
-        cleanup
 
         set +e
         grep "fno-sanitize-recover=undefined,integer" CMakeLists.txt
@@ -304,20 +270,17 @@ echo "here"
 
     elif [ "$BUILD" = "cmake-memsan" ]; then
         check_env CC MAKE
-        cleanup
 
         cmake -D CMAKE_BUILD_TYPE:String=MemSan .
         ${MAKE}
 
     elif [ "$BUILD" = "cmake-release" ]; then
         check_env CC MAKE
-        cleanup
 
         cmake -D CMAKE_BUILD_TYPE:String=Release .
         ${MAKE}
 
     elif [ "$BUILD" = "cmake-out-of-src" ]; then
-        cleanup
         MBEDTLS_ROOT_DIR="$PWD"
         mkdir build
         cd build
@@ -340,44 +303,20 @@ echo "here"
         fi
         ./tests/scripts/all.sh -r -k --no-yotta
 
-    elif [ "$BUILD" = "source-checks" ]; then
-        cleanup
-        msg "test: recursion.pl" # < 1s
-        tests/scripts/recursion.pl library/*.c
-
-        msg "test: freshness of generated source files" # < 1s
-        tests/scripts/check-generated-files.sh
-
-        msg "test: doxygen markup outside doxygen blocks" # < 1s
-        tests/scripts/check-doxy-blocks.pl
-
-        msg "test/build: declared and exported names" # < 3s
-        cleanup
-        tests/scripts/check-names.sh
-
-        msg "test: doxygen warnings" # ~ 3s
-        cleanup
-        tests/scripts/doxygen.sh
-        cleanup
     elif [ "$BUILD" = "cmake-debug" ]; then
-        cleanup
         cmake -D CMAKE_BUILD_TYPE:String=Debug .
 
     elif [ "$BUILD" = "make-full-config" ]; then
-        cleanup
         make CC=gcc CFLAGS='-Werror -Wall -Wextra -std=c99 -pedantic -O0 -D_DEFAULT_SOURCE' lib programs
         make CC=gcc CFLAGS='-Werror -Wall -Wextra -O0' test
 
     elif [ "$BUILD" = "make-lib" ]; then
-        cleanup
         ${MAKE} lib
 
     elif [ "$BUILD" = "make-lib-programs" ]; then
-        cleanup
         ${MAKE} lib programs
 
     elif [ "$BUILD" = "make-tests" ]; then
-        cleanup
         ${MAKE}
         ${MAKE} tests
 
@@ -385,7 +324,6 @@ echo "here"
         check_env CC MAKE
         if uname -a | grep -F Linux >/dev/null; then
             msg "build/test: make shared" # ~ 40s
-            cleanup
             ${MAKE} SHARED=1 all check
         fi
 
@@ -393,7 +331,6 @@ echo "here"
         check_env CC MAKE
         if uname -a | grep -F Linux >/dev/null; then
             msg "build/test: make shared" # ~ 40s
-            cleanup
             ${MAKE} SHARED=1 all check
         fi
 
@@ -402,7 +339,6 @@ echo "here"
 
         if uname -a | grep -F x86_64 >/dev/null; then
             msg "build: i386, make, gcc" # ~ 30s
-            cleanup
             ${MAKE} CC=${CC} CFLAGS=${CFLAGS}
         fi # x86_64
 
