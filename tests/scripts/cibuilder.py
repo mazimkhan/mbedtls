bc0a92cfb10f03cb5311064951887a5172fbbafd
Mohammad Azim Khan <Azim.Khan@arm.com>
2018-05-21 14:51:43 +0100

    WIP

--- tests/scripts/cibuilder.py
+++ tests/scripts/cibuilder.py
@@ -1,7 +1,10 @@
+#!/usr/bin/env python
 import os
+import re
 import sys
 import json
 import argparse
+import subprocess
 
 
 JSON_ROOT_KEY_TESTS = "tests"
@@ -9,12 +12,18 @@ JSON_ROOT_KEY_CAMPAIGNS = "campaigns"
 JSON_ROOT_KEY_JOBS = "jobs"
 
 
+#####################################################################
+# Test execution
+#####################################################################
+
+
 class Test(object):
     """
     """
 
     def __init__(self, config=None, set_config=[], unset_config=[],
-                 build=None, script=None, environment={}, tests=[], test_scripts={}):
+                 build=None, script=None, environment={}, tests=[],
+                 test_scripts={}):
         """
         """
         self.config = config
@@ -26,18 +35,29 @@ class Test(object):
         self.tests = tests
         self.test_scripts = test_scripts
 
-    def run_command(self, cmd):
+    def expand_vars(self, cmd):
         """
         """
-        print("Running: %s" % cmd)
-        os.system(cmd)
+        # Expand test environment variables
+        for name, value in self.environment.items():
+            cmd = re.sub("(\${name})|(\$\{{{name}}})".format(name=name), value, cmd)
+        # Expand system environment variables
+        return os.path.expandvars(cmd)
+
+    def run_command(self, cmd, environment={}):
+        """
+        """
+        self.expand_vars(cmd)
+        print(cmd)
+        # Create copy of system environment and expand and add test environment
+        env = os.environ.copy()
+        env.update({k:self.expand_vars(v) for k,v in environment.items()})
+        subprocess.call(cmd.split(), env=env)
 
     def run_with_env(self, cmd):
         """
         """
-        env_str = " ".join(["%s=%s" % (k,v) for k,v in self.environment.items()])
-        cmd = " ".join((env_str, cmd))
-        self.run_command(cmd)
+        self.run_command(cmd, environment=self.environment)
 
     def do_mbedtls_config(self):
         """
@@ -65,7 +85,7 @@ class Test(object):
         """
         """
         self.check_environment(['MAKE', 'CC'])
-        make_cmd = "{MAKE}".format(**self.environment)
+        make_cmd = self.environment.get("MAKE", "make")
         make_target = self.environment.get("MAKE_TARGET", "")
         self.run_with_env("%s clean" % make_cmd)
         self.run_with_env("%s %s" % (make_cmd, make_target))
@@ -104,6 +124,155 @@ class Test(object):
                 self.run_with_env(test)
 
 
+#####################################################################
+# CI data schema and validation
+#####################################################################
+
+
+class Schema(object):
+    _type = None
+
+    def __init__(self, name=None, is_mandatory=True, type=None):
+        self.name = name
+        self.is_mandatory = is_mandatory
+        if type:
+            self._type = type
+
+    def get_name(self): return self.name
+
+    def update_tag(self, tag):
+        return "->".join((tag, self.get_name())) if tag else self.name
+
+    def validate(self, data, tag=""):
+        tag = self.update_tag(tag)
+        if self._type == str:
+            if type(data) not in (str, unicode):
+                raise ValueError("%s Key '%s' value should be of type str or unicode" % (tag, self.get_name()))
+        elif type(data) != self._type:
+            raise ValueError("%s Key '%s' value should be of type %s" % (tag, self.get_name(), self._type))
+
+class DictSchema(Schema):
+    _schema = None
+
+    def validate(self, data, tag=""):
+        tag = self.update_tag(tag)
+        if type(data) != dict:
+            raise ValueError("%s Data type of '%s' should be dict." % (tag, self.get_name()))
+        for name, value in data.items():
+            if type(name) not in (str, unicode):
+                print("%s not str" % name)
+                raise ValueError("%s Keys in dictionary '%s' should be strings." % (tag, self.get_name()))
+            schema = self._schema(name)
+            schema.validate(value, tag)
+
+class ListSchema(Schema):
+    _schema = None
+
+    def validate(self, data, tag=""):
+        tag = self.update_tag(tag)
+        if type(data) != list:
+            raise ValueError("%s Data type of '%s' should be list." % (tag, self.get_name()))
+        for element in data:
+            self._schema.validate(element, tag)
+
+
+class AttributeSchema(Schema):
+    _attributes = {}
+
+    def validate(self, data, tag=""):
+        tag = self.update_tag(tag)
+        if type(data) != dict:
+            raise ValueError("%s Data type of '%s' should be dict." % (tag, self.get_name()))
+        for name, schema in self._attributes.items():
+            if name not in data:
+                if schema.is_mandatory:
+                    raise ValueError("%s '%s' not found in '%s'" % (tag, name, self.get_name()))
+            else:
+                schema.validate(data[name], tag)
+
+
+class StrSchema(Schema):
+    _type = str
+
+
+class StringDictSchema(DictSchema):
+    _schema = StrSchema
+
+
+class TestSchema(AttributeSchema):
+    _attributes = {
+        "environment": StringDictSchema("environment", False),
+        "build": StrSchema("build", False),
+        "script": StrSchema("script", False),
+        "tests": Schema("tests", False, list)
+    }
+
+    def validate(self, data, tag=""):
+        super(TestSchema, self).validate(data, tag)
+        tag = self.update_tag(tag)
+        if "build" not in data and "script" not in data:
+            raise ValueError("%s Neither 'build' nor 'script' present in test '%s'" % (tag, self.get_name()))
+        if "build" in data and "script" in data:
+            raise ValueError("%s Both 'build' and 'script' specified in test '%s'" % (tag, self.get_name()))
+
+
+class TestSequence(DictSchema):
+    _schema = TestSchema
+
+
+class TestListSchema(ListSchema):
+    _schema = StrSchema("test")
+
+
+class CampaignSequence(DictSchema):
+    _schema = TestListSchema
+
+
+class PlatformListSchema(ListSchema):
+    _schema = StrSchema("platform")
+
+
+class CampaignListSchema(ListSchema):
+    _schema = StrSchema("campaigns")
+
+
+class ComboSchema(AttributeSchema):
+    _attributes = {
+        "platforms": PlatformListSchema("platforms"),
+        "campaigns": CampaignListSchema("campaigns")
+    }
+
+
+class ComboList(ListSchema):
+    _schema = ComboSchema("Platform & Campaign")
+
+
+class JobSequence(DictSchema):
+    _schema = ComboList
+
+
+class TestScriptSchema(ListSchema):
+    _schema = StrSchema("test-script")
+
+
+class TestScriptSequence(DictSchema):
+    _schema = TestScriptSchema
+
+
+class RootSchema(AttributeSchema):
+    _attributes = {
+        "tests": TestSequence("tests"),
+        "campaigns": CampaignSequence("campaigns"),
+        "jobs": JobSequence("jobs"),
+        "test-scripts": TestScriptSequence("test-script", False)
+    }
+
+
+#####################################################################
+# CI data parsing
+#####################################################################
+
+
 class CIDataParser(object):
     CI_META_FILE="cijobs.json"
 
@@ -112,75 +281,12 @@ class CIDataParser(object):
                 self.CI_META_FILE)
         with open(ci_data_file, 'r') as f:
             self.ci_data = json.load(f)
-        self.validate()
 
     def validate(self):
         """
         """
-        # Validate root data type
-        assert type(self.ci_data) == dict, "CI Test data root should be a map!"
-
-        # Validate root keys
-        for root_key in ("tests", "campaigns", "jobs"):
-            assert root_key in self.ci_data, \
-                "Mandatory root key '%s' not present!" % root_key
-
-        # Validate tests
-        assert type(self.ci_data["tests"]) == dict, "'tests' data is not a map!"
-        for test_name, test_data in self.ci_data["tests"].items():
-            assert type(test_data) == dict, "Test '%s' data is not a map!" % test_name
-            assert "build" in test_data or "script" in test_data, \
-                "Test '%s' should specify either 'build' or 'script'" % test_name
-            if "config" in test_data:
-                assert type(test_data["config"]) == dict, \
-                    "Test '%s' config data shoud be a map!" % test_data
-                for key in test_data["config"].keys():
-                    assert key in ("config", "set", "unset"), \
-                        "Test '%s' 'config' element '%s' is unknown" % (test_name, key)
-                if "set" in test_data["config"]:
-                    assert type(test_data["config"]["set"]) == list, \
-                        "Test '%s' 'config' element 'set' is not a list" % test_name
-                if "unset" in test_data["config"]:
-                    assert type(test_data["config"]["unset"]) == list, \
-                        "Test '%s' 'config' element 'unset' is not a list" % test_name
-            if "environment" in test_data:
-                assert type(test_data["environment"]) == dict, \
-                    "Test '%s' 'environment' is not a map" % test_name
-            if "tests" in test_data:
-                assert type(test_data["tests"]) == list, \
-                    "Test '%s' 'tests' is not a list" % test_name
-
-        # Validate Campaigns
-        assert type(self.ci_data["campaigns"]) == dict, "'campaigns' data is not a map!"
-        for campaign_name, campaign in self.ci_data["campaigns"].items():
-            assert type(campaign) == list, \
-                "Campaign '%s' type is not a list!" % campaign_name
-            for test_name in campaign:
-                assert test_name in self.ci_data["tests"], \
-                    "Test '%s' reference in campaign '%s' not found in \"tests\"" % \
-                    (test_name, campaign_name)
-
-        # Validate Jobs
-        assert type(self.ci_data["jobs"]) == dict, "'jobs' data is not a map!"
-        for job_name, job in self.ci_data["jobs"].items():
-            assert type(job) == list, "Job '%s' type is not a list!" % job_name
-            for combo in job:
-                # Check mandatory keys in job
-                for mandatory_key in ("campaigns", "platforms"):
-                    assert mandatory_key in combo, \
-                        "Mandatory key '%s' not present in job!" % \
-                        (mandatory_key, job_name)
-                # Check campaigns
-                assert type(combo["campaigns"]) == list, \
-                    "Element \"campaigns\" in job '%s' is not a list!" % job_name
-                # Check campaigns exist
-                for campaign_name in combo["campaigns"]:
-                    assert campaign_name in self.ci_data["campaigns"], \
-                        "Campaign '%s' referenced by job '%s' not found!" % \
-                        (campaign_name, job_name)
-                # Check platforms
-                assert type(combo["platforms"]) == list, \
-                    "Element \"platforms\" in job '%s' is not a list!" % job_name
+        vr = RootSchema("root")
+        vr.validate(self.ci_data)
 
     def get_campaigns(self):
         return sorted(self.ci_data[JSON_ROOT_KEY_CAMPAIGNS].keys())
@@ -227,6 +333,11 @@ class CIDataParser(object):
                     test_scripts=self.ci_data.get("test-scripts", {}))
 
 
+#####################################################################
+# Command line option parsers
+#####################################################################
+
+
 def test_opt_handler(args):
     """
     """
@@ -265,28 +376,42 @@ if __name__=='__main__':
     subparsers = parser.add_subparsers(help='For help run: %(prog)s <sub-command> -h')
 
     # Parser for campaign options
-    campaign_parser = subparsers.add_parser('campaigns', help='Campaign options')
-    campaign_parser.add_argument('-l', '--list', action='store_true',
-                                 dest='list_campaigns', help='List campaigns')
-    campaign_parser.add_argument('-t', '--list-tests', dest='list_tests',
-                                 metavar="CAMPAIGN_NAME",
-                                 help='List tests in a campaign')
+    campaign_parser = subparsers.add_parser('campaigns',
+        help='Campaign options')
+    campaign_parser.add_argument('-l', '--list',
+        action='store_true',
+        dest='list_campaigns',
+        help='List campaigns')
+    campaign_parser.add_argument('-t', '--list-tests',
+        dest='list_tests',
+        metavar="CAMPAIGN_NAME",
+        help='List tests in a campaign')
     campaign_parser.set_defaults(func=campaign_opt_handler)
 
     # Parser for job options
-    job_parser = subparsers.add_parser('jobs', help='Job options')
-    job_parser.add_argument('-l', '--list', action='store_true',
-                            dest='list_jobs', help='List jobs')
-    job_parser.add_argument('-t', '--list-tests', dest='list_tests',
-                            metavar="JOB_NAME", help='List tests in a job')
+    job_parser = subparsers.add_parser('jobs',
+        help='Job options')
+    job_parser.add_argument('-l', '--list',
+        action='store_true',
+        dest='list_jobs',
+        help='List jobs')
+    job_parser.add_argument('-t', '--list-tests',
+        dest='list_tests',
+        metavar="JOB_NAME",
+        help='List tests in a job')
     job_parser.set_defaults(func=job_opt_handler)
 
     # Parser for test options
-    test_parser = subparsers.add_parser('test', help='Test options')
-    test_parser.add_argument('-l', '--list', action='store_true',
-                             dest='list_tests', help='List tests')
-    test_parser.add_argument('-r', '--run-test', dest='run_test',
-                             metavar="TEST_NAME", help='Run a test')
+    test_parser = subparsers.add_parser('test',
+        help='Test options')
+    test_parser.add_argument('-l', '--list',
+        action='store_true',
+        dest='list_tests',
+        help='List tests')
+    test_parser.add_argument('-r', '--run-test',
+        dest='run_test',
+        metavar="TEST_NAME",
+        help='Run a test')
     test_parser.set_defaults(func=test_opt_handler)
 
 
